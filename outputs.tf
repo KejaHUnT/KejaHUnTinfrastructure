af52d98b0b77a60b2a5375f4f8146e0287da0f43
Kalukwo <123067924+Kalukwo@users.noreply.github.com>
2025-06-18 18:00:22 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "instance_id" {
   description = "The ID of the EC2 instance"
   value       = module.aws_ec2.instance_id
 }
-
-output "instance_public_dns" {
-  description = "The public DNS of the EC2 instance"
-  value       = aws_instance.this.public_dns
-}
